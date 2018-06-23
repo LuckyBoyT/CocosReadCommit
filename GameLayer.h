0ac3f0eee84706b99abd9bbb872759d1aae434cd
LuckyBoyT <40432273+LuckyBoyT@users.noreply.github.com>
2018-06-23 15:37:18 +0800

    Create GameLayer.h

---
+++ GameLayer.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include "cocos2d.h"
+using namespace std;
+USING_NS_CC;
+
+class GameLayer : Layer
+{
+public:
+	GameLayer();
+	~GameLayer();	
+  
+	CREATE_FUNC(GameLayer);
+};
+
